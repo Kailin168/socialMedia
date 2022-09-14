5ca4c12f3b60e2344c7eb56ff371f4a8edfe7837
Kai L <kailin8591@gmail.com>
2022-09-14 13:07:29 -0400

    testing

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import actionCable from 'actioncable';
 
 import AuthProvider from './contexts/AuthContext';
 import './index.css';
@@ -9,11 +10,13 @@ import App from './App';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
+const CableApp = {};
+CableApp.cable = actionCable.createConsumer('ws://localhost:3000/cable');
 
 root.render(
   <BrowserRouter>
     <AuthProvider>
-      <App />
+      <App cable={CableApp.cable} />
     </AuthProvider>
   </BrowserRouter>,
 );
