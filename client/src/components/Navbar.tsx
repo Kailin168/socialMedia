b7de55908d51d3626c85a3e5bf15cf7f6e61bb34
Kai L <kailin8591@gmail.com>
2022-09-14 10:51:52 -0400

    minor css fix

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -14,10 +14,10 @@ export default function Navbar() {
   return (
     <div>
       <div>
-        <div className="flex text-whiten justify-center bg-blue-500 mx-auto text-lg">
+        <div className="flex text-white justify-center bg-blue-500 mx-auto text-lg">
           <div className="w-1/3 flex items-center relative title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900"><img src="/title.png" alt="ad" className="ml-4 items-center w-60 mt-4" /></div>
           <div className="w-1/3 min-w-fit flex justify-center">
-            <ul className="flex text-white bg-blue-500 mx-auto text-lg">
+            <ul className="flex text-white bg-blue-500 mx-auto text-2xl">
               <button type="button" onClick={() => navigate('/')} className="py-5 px-10 font-semibold hover:text-slate-900 transition duration-300">Home</button>
               <button type="button" onClick={() => navigate('/message')} className="py-5 px-10 font-semibold hover:text-slate-900 transition duration-300">Message</button>
               <button type="button" onClick={() => navigate('/likes')} className="py-5 px-10 font-semibold hover:text-slate-900 transition duration-300">Likes</button>
